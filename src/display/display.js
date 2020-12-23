c61c4c7005fbac4d632d02b36b1603a860585d44
AJ Olson <silentx.alex@gmail.com>
2020-12-23 16:14:45 -0500

    added transitions to expense table on add and removal

--- src/display/display.js
+++ src/display/display.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { AssessmentOutlined, AttachMoneyOutlined, ReceiptOutlined, RemoveCircleOutline } from '@material-ui/icons';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 class Display extends Component {
 
@@ -9,49 +10,51 @@ class Display extends Component {
         const total = expenses.reduce((sum, curr) => sum + curr.amount, 0);
         const difference = income - total || 0;
         return (
-                    <table>
-                        <thead>
-                            <tr>
-                                <td className="income_display">
-                                    <h3>Income</h3>
-                                    <AttachMoneyOutlined style={{fontSize: 40, color: 'green'}} />
-                                    <p>${income || 0}</p>
+            <table>
+                <thead>
+                    <tr>
+                        <td className="income_display">
+                            <h3>Income</h3>
+                            <AttachMoneyOutlined style={{ fontSize: 40, color: 'green' }} />
+                            <p>${income || 0}</p>
+                        </td>
+                        <td className="expenses_display">
+                            <h3>Expenses</h3>
+                            <ReceiptOutlined style={{ fontSize: 40, color: '#2489db' }} />
+                            <p>${total}</p>
+                        </td>
+                        <td className="diff_display">
+                            <h3>Difference</h3>
+                            <AssessmentOutlined style={{ fontSize: 40 }} />
+                            <p style={difference >= 0 ? { color: 'blue' } : { color: 'red' }}>${difference}</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><strong>Expense Name</strong></td>
+                        <td><strong>Expense Amount</strong></td>
+                        <td><strong>Remove</strong></td>
+                    </tr>
+                </thead>
+                <TransitionGroup component="tbody">
+                    {expenses && expenses.map((expense, index) => (
+                        <CSSTransition key={index} timeout={200} classNames="fade">
+                            <tr key={index} className="expense">
+                                <td>
+                                    {expense.name}
                                 </td>
-                                <td className="expenses_display">
-                                    <h3>Expenses</h3>
-                                    <ReceiptOutlined style={{fontSize: 40, color: '#2489db'}} />
-                                    <p>${total}</p>
+                                <td>
+                                    ${expense.amount}
                                 </td>
-                                <td className="diff_display">
-                                    <h3>Difference</h3>
-                                    <AssessmentOutlined style={{fontSize: 40}} />
-                                    <p style={difference >= 0 ? {color: 'blue'} : {color: 'red'}}>${difference}</p>
+                                <td>
+                                    <button onClick={() => this.props.removeExpense(index)} className="remove">
+                                        <RemoveCircleOutline />
+                                    </button>
                                 </td>
                             </tr>
-                            <tr>
-                                <td><strong>Expense Name</strong></td>
-                                <td><strong>Expense Amount</strong></td>
-                                <td><strong>Remove</strong></td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {expenses && expenses.map((expense, index) => {
-                                return <tr key={index} className="expense">
-                                    <td>
-                                        {expense.name}
-                                    </td>
-                                    <td>
-                                        ${expense.amount}
-                                    </td>
-                                    <td>
-                                        <button onClick={() => this.props.removeExpense(index)} className="remove">
-                                            <RemoveCircleOutline />
-                                        </button>
-                                    </td>
-                                </tr>
-                            })}
-                        </tbody>
-                    </table>
+                        </CSSTransition>
+                    ))}
+                </TransitionGroup>
+            </table>
         )
     }
 }
